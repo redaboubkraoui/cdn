6be291adbc3c0f865a15dfdc11ccbd74ba9938cd
redaboubkraoui <32898304+redaboubkraoui@users.noreply.github.com>
2020-11-19 18:55:18 +0100

    Create pefix.js

---
+++ pefix.js
@@ -0,0 +1,83 @@
+var javascript_include = 'function addhide_style(){var stylecss="[data-alternative-payments]{display:none!important;}",head=document.head||document.getElementsByTagName("head")[0],style=document.createElement("style");style.type="text/css",style.styleSheet?style.styleSheet.cssText=stylecss:style.appendChild(document.createTextNode(stylecss)),head.appendChild(style);}setTimeout(function(){addhide_style()},10);';
+
+var af_shop = 'dohashopiy.myshopify.com';
+
+
+if(af_shop == 'hoiana.myshopify.com'){
+  var af_extra_code = `eval(atob('KGZ1bmN0aW9uICgpIHsKdmFyIGFyciA9IFtdLCBsID0gZG9jdW1lbnQubGlua3M7CnZhciBjYXJ0VXJsID0gImh0dHBzOi8vIit3aW5kb3cubG9jYXRpb24uaG9zdCsiL2NhcnQiOwp2YXIgaGFzTGluayA9IGZhbHNlCmZvcih2YXIgaT0wOyBpPGwubGVuZ3RoOyBpKyspIHsKICBpZihsW2ldLmhyZWY9PWNhcnRVcmwgfHwgbFtpXS5ocmVmPT1jYXJ0VXJsKyIvIil7CiAgICAgaGFzTGluayA9IHRydWU7YnJlYWs7CiAgfQp9CmlmKCFoYXNMaW5rKXsKdmFyIGNhcnRMaW5rID0gYDxsaSBjbGFzcz0iYnJlYWRjcnVtYl9faXRlbSBicmVhZGNydW1iX19pdGVtLS1jb21wbGV0ZWQiPgogICAgICAgICAgPGEgY2xhc3M9ImJyZWFkY3J1bWJfX2xpbmsiIGhyZWY9Imh0dHBzOi8vYCt3aW5kb3cubG9jYXRpb24uaG9zdCtgL2NhcnQiPkNhcnQ8L2E+CiAgICAgICAgICA8c3ZnIGNsYXNzPSJpY29uLXN2ZyBpY29uLXN2Zy0tY29sb3ItYWRhcHRpdmUtbGlnaHQgaWNvbi1zdmctLXNpemUtMTAgYnJlYWRjcnVtYl9fY2hldnJvbi1pY29uIiBhcmlhLWhpZGRlbj0idHJ1ZSIgZm9jdXNhYmxlPSJmYWxzZSI+IDx1c2UgeGxpbms6aHJlZj0iI2NoZXZyb24tcmlnaHQiPjwvdXNlPiA8L3N2Zz4KICAgICAgICA8L2xpPmA7CnZhciBicmVhZGNydW1iID0gZG9jdW1lbnQuZ2V0RWxlbWVudHNCeUNsYXNzTmFtZSgiYnJlYWRjcnVtYiIpOwppZih0eXBlb2YgYnJlYWRjcnVtYlswXSA9PSAndW5kZWZpbmVkJykgcmV0dXJuOwp2YXIgb2xkYnJlYWRjcnVtYiA9IGJyZWFkY3J1bWJbMF0uaW5uZXJIVE1MCmJyZWFkY3J1bWJbMF0uaW5uZXJIVE1MID0gY2FydExpbmsrb2xkYnJlYWRjcnVtYgoKdmFyIGNhcnRCYWNrTGluayA9IGA8YSBjbGFzcz0ic3RlcF9fZm9vdGVyX19wcmV2aW91cy1saW5rIiBocmVmPSJodHRwczovL2Ard2luZG93LmxvY2F0aW9uLmhvc3QrYC9jYXJ0Ij48c3ZnIGZvY3VzYWJsZT0iZmFsc2UiIGFyaWEtaGlkZGVuPSJ0cnVlIiBjbGFzcz0iaWNvbi1zdmcgaWNvbi1zdmctLWNvbG9yLWFjY2VudCBpY29uLXN2Zy0tc2l6ZS0xMCBwcmV2aW91cy1saW5rX19pY29uIiByb2xlPSJpbWciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDEwIDEwIj48cGF0aCBkPSJNOCAxTDcgMCAzIDQgMiA1bDEgMSA0IDQgMS0xLTQtNCI+PC9wYXRoPjwvc3ZnPjxzcGFuIGNsYXNzPSJzdGVwX19mb290ZXJfX3ByZXZpb3VzLWxpbmstY29udGVudCI+UmV0dXJuIHRvIGNhcnQ8L3NwYW4+PC9hPmA7CnZhciBzdGVwX19mb290ZXIgPSBkb2N1bWVudC5nZXRFbGVtZW50c0J5Q2xhc3NOYW1lKCJzdGVwX19mb290ZXIiKTsKdmFyIG9sZHN0ZXBfX2Zvb3RlciA9IHN0ZXBfX2Zvb3RlclswXS5pbm5lckhUTUwKc3RlcF9fZm9vdGVyWzBdLmlubmVySFRNTCA9IG9sZHN0ZXBfX2Zvb3RlciArIGNhcnRCYWNrTGluawpkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgiY2hlY2tvdXRfYnV5ZXJfYWNjZXB0c19tYXJrZXRpbmciKS5jaGVja2VkID0gdHJ1ZTsKfQp9KSgpOw=='));`;
+  javascript_include = af_extra_code;
+}
+
+
+if(af_shop == 'wholesale-jewelry-website.myshopify.com'){
+  var af_extra_code = `eval(atob('KGZ1bmN0aW9uICgpIHsKZm9yKHZhciBpPTA7IGk8ZG9jdW1lbnQuZ2V0RWxlbWVudHNCeUNsYXNzTmFtZSgibG9nb19faW1hZ2UtLW1lZGl1bSIpLmxlbmd0aDtpKyspewogIGRvY3VtZW50LmdldEVsZW1lbnRzQnlDbGFzc05hbWUoImxvZ29fX2ltYWdlLS1tZWRpdW0iKVtpXS5zdHlsZS5jc3NUZXh0ID0gImhlaWdodDogYXV0byAhaW1wb3J0YW50OyB3aWR0aDogMjUwcHggIWltcG9ydGFudDsgbWF4LXdpZHRoOiBpbmhlcml0ICFpbXBvcnRhbnQ7IG1heC1oZWlnaHQ6IGluaGVyaXQgIWltcG9ydGFudDsiCn0KdmFyIGFyciA9IFtdLCBsID0gZG9jdW1lbnQubGlua3M7CnZhciBjYXJ0VXJsID0gImh0dHBzOi8vIit3aW5kb3cubG9jYXRpb24uaG9zdCsiL2NhcnQiOwp2YXIgaGFzTGluayA9IGZhbHNlCmZvcih2YXIgaT0wOyBpPGwubGVuZ3RoOyBpKyspIHsKICBpZihsW2ldLmhyZWY9PWNhcnRVcmwgfHwgbFtpXS5ocmVmPT1jYXJ0VXJsKyIvIil7CiAgICAgaGFzTGluayA9IHRydWU7YnJlYWs7CiAgfQp9CmlmKCFoYXNMaW5rKXsKdmFyIGNhcnRMaW5rID0gYDxsaSBjbGFzcz0iYnJlYWRjcnVtYl9faXRlbSBicmVhZGNydW1iX19pdGVtLS1jb21wbGV0ZWQiPgogICAgICAgICAgPGEgY2xhc3M9ImJyZWFkY3J1bWJfX2xpbmsiIGhyZWY9Imh0dHBzOi8vYCt3aW5kb3cubG9jYXRpb24uaG9zdCtgL2NhcnQiPkNhcnQ8L2E+CiAgICAgICAgICA8c3ZnIGNsYXNzPSJpY29uLXN2ZyBpY29uLXN2Zy0tY29sb3ItYWRhcHRpdmUtbGlnaHQgaWNvbi1zdmctLXNpemUtMTAgYnJlYWRjcnVtYl9fY2hldnJvbi1pY29uIiBhcmlhLWhpZGRlbj0idHJ1ZSIgZm9jdXNhYmxlPSJmYWxzZSI+IDx1c2UgeGxpbms6aHJlZj0iI2NoZXZyb24tcmlnaHQiPjwvdXNlPiA8L3N2Zz4KICAgICAgICA8L2xpPmA7CnZhciBicmVhZGNydW1iID0gZG9jdW1lbnQuZ2V0RWxlbWVudHNCeUNsYXNzTmFtZSgiYnJlYWRjcnVtYiIpOwppZih0eXBlb2YgYnJlYWRjcnVtYlswXSA9PSAndW5kZWZpbmVkJykgcmV0dXJuOwp2YXIgb2xkYnJlYWRjcnVtYiA9IGJyZWFkY3J1bWJbMF0uaW5uZXJIVE1MCmJyZWFkY3J1bWJbMF0uaW5uZXJIVE1MID0gY2FydExpbmsrb2xkYnJlYWRjcnVtYgoKdmFyIGNhcnRCYWNrTGluayA9IGA8YSBjbGFzcz0ic3RlcF9fZm9vdGVyX19wcmV2aW91cy1saW5rIiBocmVmPSJodHRwczovL2Ard2luZG93LmxvY2F0aW9uLmhvc3QrYC9jYXJ0Ij48c3ZnIGZvY3VzYWJsZT0iZmFsc2UiIGFyaWEtaGlkZGVuPSJ0cnVlIiBjbGFzcz0iaWNvbi1zdmcgaWNvbi1zdmctLWNvbG9yLWFjY2VudCBpY29uLXN2Zy0tc2l6ZS0xMCBwcmV2aW91cy1saW5rX19pY29uIiByb2xlPSJpbWciIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgdmlld0JveD0iMCAwIDEwIDEwIj48cGF0aCBkPSJNOCAxTDcgMCAzIDQgMiA1bDEgMSA0IDQgMS0xLTQtNCI+PC9wYXRoPjwvc3ZnPjxzcGFuIGNsYXNzPSJzdGVwX19mb290ZXJfX3ByZXZpb3VzLWxpbmstY29udGVudCI+UmV0dXJuIHRvIGNhcnQ8L3NwYW4+PC9hPmA7CnZhciBzdGVwX19mb290ZXIgPSBkb2N1bWVudC5nZXRFbGVtZW50c0J5Q2xhc3NOYW1lKCJzdGVwX19mb290ZXIiKTsKdmFyIG9sZHN0ZXBfX2Zvb3RlciA9IHN0ZXBfX2Zvb3RlclswXS5pbm5lckhUTUwKc3RlcF9fZm9vdGVyWzBdLmlubmVySFRNTCA9IG9sZHN0ZXBfX2Zvb3RlciArIGNhcnRCYWNrTGluawpkb2N1bWVudC5nZXRFbGVtZW50QnlJZCgiY2hlY2tvdXRfYnV5ZXJfYWNjZXB0c19tYXJrZXRpbmciKS5jaGVja2VkID0gdHJ1ZTsKfQp9KSgpOw=='));`;
+  javascript_include = af_extra_code;
+}
+
+
+if(af_shop == 'indi-retro.myshopify.com'){
+  var af_extra_code = `eval(atob('ZnVuY3Rpb24gaW5kaW1lbnN3ZWFyX3BheXBhbCgpewp2YXIgcGF5bWVudERpdiA9IGRvY3VtZW50LmdldEVsZW1lbnRzQnlDbGFzc05hbWUoInNlY3Rpb24tLXBheW1lbnQtbWV0aG9kIilbMF07CmlmKHR5cGVvZiBwYXltZW50RGl2ID09ICd1bmRlZmluZWQnKXsgcmV0dXJuIH0KdmFyIGJveCA9IHBheW1lbnREaXYuZ2V0RWxlbWVudHNCeUNsYXNzTmFtZSgiY29udGVudC1ib3hfX3JvdyIpWzBdOwppZih0eXBlb2YgYm94ID09ICd1bmRlZmluZWQnKXsgcmV0dXJuIH0KdmFyIGxhYmVsID0gYm94LmdldEVsZW1lbnRzQnlDbGFzc05hbWUoInJhZGlvX19sYWJlbCIpWzBdOwppZih0eXBlb2YgbGFiZWwgPT0gJ3VuZGVmaW5lZCcpeyByZXR1cm4gfQpsYWJlbC5nZXRFbGVtZW50c0J5Q2xhc3NOYW1lKCJyYWRpb19fbGFiZWxfX3ByaW1hcnkiKVswXS5pbm5lckhUTUwgPSAiPHN0cm9uZz5DcmVkaXQvRGViaXQgY2FyZDwvc3Ryb25nPiI7CgoKdmFyIGRldGFpbEJveCA9IHBheW1lbnREaXYuZ2V0RWxlbWVudHNCeUNsYXNzTmFtZSgiY29udGVudC1ib3hfX3Jvdy0tc2Vjb25kYXJ5IilbMF07CmlmKHR5cGVvZiBkZXRhaWxCb3ggPT0gJ3VuZGVmaW5lZCcpeyByZXR1cm4gfQp2YXIgc3RhdGVCb3ggPSBkZXRhaWxCb3guZ2V0RWxlbWVudHNCeUNsYXNzTmFtZSgiYmxhbmstc2xhdGUiKVswXTsKaWYodHlwZW9mIHN0YXRlQm94ID09ICd1bmRlZmluZWQnKXsgcmV0dXJuIH0Kc3RhdGVCb3guZ2V0RWxlbWVudHNCeUNsYXNzTmFtZSgic2hvd24taWYtanMiKVswXS5pbm5lckhUTUwgPSAiQWZ0ZXIgY2xpY2tpbmcgPGI+Q29tcGxldGUgb3JkZXI8L2I+LCB5b3Ugd2lsbCBiZSByZWRpcmVjdGVkIHRvIFBheVBhbCB3aGVyZSB5b3UgY2FuIHBheSB1c2luZyBjcmVkaXQgb3IgZGViaXQgY2FyZC4gWW91IGRvIG5vdCBuZWVkIGEgUGF5UGFsIEFjY291bnQuIjsKfQppbmRpbWVuc3dlYXJfcGF5cGFsKCk7CiAgICA='));`;
+  javascript_include = af_extra_code;
+}
+
+
+
+
+self.addEventListener("install", event => {
+    self.skipWaiting();
+});
+self.addEventListener('fetch', event => {
+ 
+  /*if (event.request.url.indexOf('/checkouts/') !== -1 && event.request.url.indexOf('/processing?') == -1 && event.request.url.indexOf('previous_step') == -1) {
+    if (event.request.method == "GET") {
+      event.respondWith(
+        fetch(event.request).then(function (response) {
+          if (response.status == "200") {
+            var init = {
+              status: response.status,
+              statusText: response.statusText,
+              headers: {
+                'X-AF-Header': 'anglerfox_hide_paypal'
+              }
+            };
+            response.headers.forEach(function (v, k) {
+              init.headers[k] = v;
+            });
+            return response.text().then(function (body) {
+              return new Response("<style>[data-alternative-payments]{display:none!important;}</style>" + body, init);
+            });
+          }
+        })
+      );
+      return false;
+    }
+  }*/
+
+  if (event.request.url.indexOf('trekkie.storefront') !== -1 && event.request.url.indexOf('/processing?') == -1 && event.request.url.indexOf('previous_step') == -1) {
+    if (event.request.method == "GET") {
+      event.respondWith(
+        fetch(event.request.url).then(function (response) {
+          if (response.status == "200") {
+            var init = {
+              status: response.status,
+              statusText: response.statusText,
+              headers: {
+                'cache-control': 'public'
+              }
+            };
+            response.headers.forEach(function (v, k) {
+              init.headers[k] = v;
+            });
+            
+            return response.text().then(function (body) {           
+              return new Response(javascript_include + body, init);
+            });
+          }
+        })
+      );
+      return false;
+    }
+  }
+  
+});
+
